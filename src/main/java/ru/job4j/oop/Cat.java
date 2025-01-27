69a41f127b813119c7c77c9fbcb2723d8a8c32c8
Артур Нагиев <nagiev-art@mail.ru>
2025-01-27 21:15:26 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,33 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        name = nick;
+    }
+
+    public void show() {
+        System.out.println(name);
+        System.out.println(food);
+    }
+
+    public void eat(String meat) {
+        food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat pappy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("There is gav's food.");
+        Cat gav = new Cat();
+        gav.eat("cutlet");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("There is black's food.");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
