0d3ec9659efa66fd7336c8b1dd1a69f6d2242c1c
Артур Нагиев <nagiev-art@mail.ru>
2025-02-16 19:29:36 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        LocalDateTime dateInFormat = item.getCreated();
+        System.out.println(dateInFormat.format(formatter));
+    }
+
+}
