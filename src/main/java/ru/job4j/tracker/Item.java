9515d0e7a861b364eab67a065f51790c1f15c7bf
Артур Нагиев <nagiev-art@mail.ru>
2025-02-17 19:58:12 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,11 +5,7 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-    LocalDateTime created = LocalDateTime.now();
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
 
@@ -22,6 +18,10 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
