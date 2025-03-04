8b448aae4655f094203d0f38d13f0dda3fac20ac
Артур Нагиев <nagiev-art@mail.ru>
2025-03-04 19:37:35 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -27,7 +27,7 @@ public class Tracker {
     public void delete(int id) {
         int index = indexOf(id);
             if (index != -1) {
-            System.arraycopy(items, indexOf(id) + 1, items, index, size - indexOf(id) - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             }
