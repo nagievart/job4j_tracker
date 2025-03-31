ef1114e711e115401038a8a2cd1b2c6f3b2194a8
Артур Нагиев <nagiev-art@mail.ru>
2025-03-31 19:44:08 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,25 +1,5 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int result = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                result = i;
-                break;
-            }
-        }
-        if (result == -1) {
-            throw new ElementNotFoundException();
-        }
-        return result;
-    }
 
-    public static void main(String[] args) {
-        try {
-            int temp = indexOf(new String[] {"Строка1", "Строка2", "Строка3"}, "Строка3");
-        } catch (ElementNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
