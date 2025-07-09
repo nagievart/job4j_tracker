8e1879770a5beb7a98ff788296c3ba9f428a1b01
Артур Нагиев <nagiev-art@mail.ru>
2025-07-09 20:58:18 +0300

    Репозиторий под проект Tracker

---
+++ src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Artur", "Nagiev", "534872", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("giev");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Nagiev");
+    }
+
+    @Test
+    public void whenNothingFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Artur", "Nagiev", "534872", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("kek");
+        assertThat(persons.isEmpty());
+    }
+}
