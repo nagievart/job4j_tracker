85f4ca6586c98a196c14cde42d7ef9d3ef361309
Артур Нагиев <nagiev-art@mail.ru>
2025-07-16 19:52:36 +0300

    -

---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<>();
+        autos.add("LADA");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        for (String auto : autos) {
+            System.out.println(auto);
+        }
+    }
+}
