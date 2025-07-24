260414d91ce75e5d8c7a3a276523c5c51226a163
Артур Нагиев <nagiev-art@mail.ru>
2025-07-24 20:00:37 +0300

    -

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> myMap = new HashMap<>();
         myMap.put("nagiev-art@mail.ru", "Нагиев А.Р.");
+        myMap.put("nagievart1995@gmail.com", "Нагиев А.Р.");
+        myMap.put("kek", "NoName User");
+        myMap.put("kek", "Нагиев А.Р.");
         for (String key : myMap.keySet()) {
             String value = myMap.get(key);
             System.out.println(key + "  =   " + value);
