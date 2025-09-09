8c39ba670feba4cb765b0b41c22f8271a9306985
Артур Нагиев <nagiev-art@mail.ru>
2025-09-09 20:01:42 +0300

    -

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,13 +5,9 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int minLength = Math.min(left.length(), right.length());
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
 
-        for (int i = 0; i < minLength; i++) {
-        char leftChar = left.charAt(i);
-        char rightChar = right.charAt(i);
-
-        int charsComparison = Character.compare(leftChar, rightChar);
+        int charsComparison = Character.compare(left.charAt(i), right.charAt(i));
 
         if (charsComparison != 0) {
             return charsComparison;
