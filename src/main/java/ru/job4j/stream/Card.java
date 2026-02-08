2dad461d8cc2f07baba062e8485bb0f367e07e60
Артур Нагиев <nagiev-art@mail.ru>
2026-02-08 11:18:25 +0300

    -

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import ru.job4j.stream.Suit;
+import ru.job4j.stream.Value;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        String[] suits = {Suit.Clubs.toString(), Suit.Hearts.toString(),
+                Suit.Spades.toString(), Suit.Diamonds.toString()};
+        String[] values = {Value.V_6.toString(), Value.V_7.toString(), Value.V_8.toString()};
+
+        Stream.of(suits)
+                .flatMap(suit -> Stream.of(values)
+                        .map(value -> value + " " + suit))
+                .forEach(System.out::println);
+    }
+}
