4b7d0ebbfefcfa8cb23955d4776d9d5749a334d3
Артур Нагиев <nagiev-art@mail.ru>
2026-02-22 10:24:46 +0300

    -

--- src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
@@ -10,7 +10,7 @@ class PhoneDictionaryTest {
 
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
@@ -30,7 +30,7 @@ class PhoneDictionaryTest {
 
     @Test
     public void whenNothingFound() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Artur", "Nagiev", "534872", "Moscow")
         );
